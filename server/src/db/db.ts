92550c79f277bb9eca3e486a7b5d84d37c320149
Godswillben <amogugodswill99@gmail.com>
2022-01-02 15:38:16 +0100

    Honestly don't know what to say frontend

--- server/src/db/db.ts
+++ server/src/db/db.ts
@@ -2,8 +2,8 @@
 const PGPool = require("pg").Pool;
 
 const pgPool = new PGPool({
-    user:"postgres",
-    password: "kingdom123Freedom",
+    user:"",
+    password: "",
     host: "localhost",
     port: 5432,
     database: "social_market"
